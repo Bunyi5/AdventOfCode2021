6a664d81f5f6367fa9d5c6e5976151f7a4ff7ae5
BalintBakos <balint.bakos@theitsolutions.io>
2022-08-24 15:48:21 +0200

    Init task16, task17

---
+++ src/main/java/advent/of/code/tasks/task16/Task16.java
@@ -0,0 +1,14 @@
+package advent.of.code.tasks.task16;
+
+import advent.of.code.helper.RunType;
+
+public class Task16 {
+
+    public static void main(String[] args) {
+        runMain(RunType.TEST);
+    }
+
+    private static void runMain(RunType runType) {
+
+    }
+}
---
+++ src/main/java/advent/of/code/tasks/task17/Task17.java
@@ -0,0 +1,14 @@
+package advent.of.code.tasks.task17;
+
+import advent.of.code.helper.RunType;
+
+public class Task17 {
+
+    public static void main(String[] args) {
+        runMain(RunType.TEST);
+    }
+
+    private static void runMain(RunType runType) {
+
+    }
+}
